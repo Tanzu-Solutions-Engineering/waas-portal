723b978fa2d22fa60c82e80d68e72d3eeb6cef6e
Chris DeLashmutt <cdelashmutt@pivotal.io>
2022-04-20 11:50:45 -0400

    Fix js imports

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,17 +1,17 @@
 import "./style.scss";
 
-import "@cds/core/alert/register.js";
-import "@cds/core/button/register.js";
+import "../frontend/node_modules/@cds/core/alert/register.js";
+import "../frontend/node_modules/@cds/core/button/register.js";
 
-import "@cds/core/icon/register.js";
-import { ClarityIcons, nodeGroupIcon, gridViewIcon, plusCircleIcon } from "@cds/core/icon";
+import "../frontend/node_modules/@cds/core/icon/register.js";
+import { ClarityIcons, nodeGroupIcon, gridViewIcon, plusCircleIcon } from "../frontend/node_modules/@cds/core/icon";
 
 ClarityIcons.addIcons(nodeGroupIcon, gridViewIcon, plusCircleIcon);
 
-import "@cds/core/card/register.js";
-import "@cds/core/navigation/register.js";
+import "../frontend/node_modules/@cds/core/card/register.js";
+import "../frontend/node_modules/@cds/core/navigation/register.js";
 
-import "@cds/core/input/register.js";
-import "@cds/core/forms/register.js"
-import "@cds/core/datalist/register.js";
-import "@cds/core/select/register.js";
+import "../frontend/node_modules/@cds/core/input/register.js";
+import "../frontend/node_modules/@cds/core/forms/register.js"
+import "../frontend/node_modules/@cds/core/datalist/register.js";
+import "../frontend/node_modules/@cds/core/select/register.js";
