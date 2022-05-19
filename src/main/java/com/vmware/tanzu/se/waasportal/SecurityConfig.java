64995d94506c373fbec9f72a560ba00047f547d5
Chris DeLashmutt <cdelashmutt@pivotal.io>
2022-05-19 13:30:22 -0400

    Update config to allow readiness/liveness

--- src/main/java/com/vmware/tanzu/se/waasportal/SecurityConfig.java
+++ src/main/java/com/vmware/tanzu/se/waasportal/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.vmware.tanzu.se.waasportal;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                    .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                    .antMatchers("/*.css", "/*.js").permitAll()
+                    .requestMatchers(
+                        EndpointRequest.to("health","info"), 
+                        EndpointRequest.toLinks()).permitAll()
+                    .antMatchers("/livez", "/readyz").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .oauth2Login();
